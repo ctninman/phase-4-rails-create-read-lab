78349c6a1cde8bd87fd1fd5f836d9a99b69102d8
ctninman <89664157+ctninman@users.noreply.github.com>
2022-02-22 16:39:47 -0500

    commit tree 37c3993c253c2340115f095c1783947dd98949b0

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,30 @@
+class PlantsController < ApplicationController
+
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  # POST /birds
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  # GET /birds/:id
+  def show
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+
+end
